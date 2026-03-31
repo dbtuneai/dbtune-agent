74d79025289a7cff73fdc48b89152874f5363672
David Edwards <david@dbtune.com>
2026-03-31 13:24:33 +0200

    feat: add pg_database collector for AV

---
+++ pkg/pg/queries/pg_database.go
@@ -0,0 +1,37 @@
+package queries
+
+// https://www.postgresql.org/docs/current/catalog-pg-database.html
+
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	PgDatabaseName     = "pg_database"
+	PgDatabaseInterval = 1 * time.Minute
+)
+
+const pgDatabaseQuery = `
+SELECT
+	d.oid,
+	d.datname,
+	d.datistemplate,
+	d.datfrozenxid,
+	d.datminmxid
+FROM pg_database d
+`
+
+// PgDatabaseRow represents a single row from pg_database.
+type PgDatabaseRow struct {
+	Oid        Oid     `json:"oid"`
+	DataName   Name    `json:"datname"`
+	IsTemplate Boolean `json:"datistemplate"`
+	FrozenXID  Xid     `json:"datfrozenxid"`
+	MinXID     Xid     `json:"datminmxid"`
+}
+
+func PgDatabaseCollector(pool *pgxpool.Pool, prepareCtx PrepareCtx) CatalogCollector {
+	return NewCollector[PgDatabaseRow](pool, prepareCtx, PgDatabaseName, PgDatabaseInterval, pgDatabaseQuery)
+}
